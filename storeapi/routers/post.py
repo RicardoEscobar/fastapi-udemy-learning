f047b2434e3dd2ef447814f134e2d171b8c2afb2
Ricardo Escobar <jorge.ricardo.escobar@gmail.com>
2026-01-03 14:11:12 -0600

    Fix duplicate endpoint definition for retrieving post with comments

--- storeapi/routers/post.py
+++ storeapi/routers/post.py
@@ -31,9 +31,24 @@ async def create_comment(comment: CommentIn):
     last_record_id = len(comment_table)
     new_comment = {**data, "id": last_record_id}
     comment_table[last_record_id] = new_comment
-    return new_comment
+    return new_comment@router.get("/post/{post_id}", response_model=UserPostWithComments)
+async def get_post_with_comments(post_id: int):
+    post = find_post(post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    
+    return {"post": post, "comments": await get_comments_on_post(post_id)}
 
 
 @router.get("/post/{post_id}/comment", response_model=list[Comment])
 async def get_comments_on_post(post_id: int):
     return [comment for comment in comment_table.values() if comment["post_id"] == post_id]
+
+
+@router.get("/post/{post_id}", response_model=UserPostWithComments)
+async def get_post_with_comments(post_id: int):
+    post = find_post(post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    return {"post": post, "comments": await get_comments_on_post(post_id)}
