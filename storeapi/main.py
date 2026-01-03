b85733d73b996f4a2d78cb2a3c6a20dce67ddb36
Ricardo Escobar <jorge.ricardo.escobar@gmail.com>
2026-01-03 13:23:40 -0600

    Refactor main.py to include post router and keep main.py clean

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,25 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from storeapi.routers.post import router as post_router
 
 app = FastAPI()
-
-class UserPostIn(BaseModel):
-    body: str
-
-class UserPost(UserPostIn):
-    id: int
-
-
-post_table = {}
-
-@app.post("/", response_model=UserPost)
-async def create_post(post_in: UserPostIn):
-    data = post_in.model_dump()
-    last_record_id = len(post_table)
-    new_post = {**data, "id": last_record_id}
-    post_table[last_record_id] = new_post
-    return new_post
-
-@app.get("/", response_model=list[UserPost])
-async def get_all_posts():
-    return list(post_table.values())
+app.include_router(post_router)
