afc0779d0ea8c16b7c3403da4de15cb75c501401
Ricardo Escobar <jorge.ricardo.escobar@gmail.com>
2026-01-04 17:42:02 -0600

    Convert test functions to async using pytest-asyncio

--- storeapi/tests/test_simple.py
+++ storeapi/tests/test_simple.py
@@ -1,10 +1,15 @@
-def test_one_two():
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_one_two():
     x = 1
     y = 2
     assert x + y == 3
 
 
-def test_diff_contains():
+@pytest.mark.asyncio
+async def test_diff_contains():
     x = {'a': 1, 'b': 2}
     expected = {'a': 1}
     assert expected.items() <= x.items()
