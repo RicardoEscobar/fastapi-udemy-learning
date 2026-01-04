636d4e7c8380f21152ea7ea127223f8f691b8a0b
Ricardo Escobar <jorge.ricardo.escobar@gmail.com>
2026-01-04 16:28:43 -0600

    Add conftest.py for test fixtures and async client setup

---
+++ storeapi/tests/conftest.py
@@ -0,0 +1,31 @@
+from typing import AsyncGenerator, Generator
+
+import pytest
+from fastapi.testclient import TestClient
+from httpx import AsyncClient
+
+from storeapi.main import app
+from storeapi.routers.post import comment_table, post_table
+
+
+@pytest.fixture()
+def anyio_backend() -> str:
+    return "asyncio"
+
+
+@pytest.fixture()
+def client() -> Generator:
+    yield TestClient(app)
+
+
+@pytest.fixture(autouse=True)
+async def db():
+    post_table.clear()
+    comment_table.clear()
+    yield
+
+
+@pytest.fixture()
+async def async_client() -> AsyncGenerator:
+    async with AsyncClient(app=app, base_url=client.base_url) as ac:
+        yield ac
