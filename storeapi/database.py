5478e44f9235b74e99bac6923d5467890b55f57a
Ricardo Escobar <jorge.ricardo.escobar@gmail.com>
2026-01-08 20:06:42 -0600

    Format code for better readability in database.py

--- storeapi/database.py
+++ storeapi/database.py
@@ -1,7 +1,7 @@
 import databases
 import sqlalchemy
-from storeapi.config import config
 
+from storeapi.config import config
 
 metadata = sqlalchemy.MetaData()
 
@@ -9,7 +9,7 @@ post_table = sqlalchemy.Table(
     "posts",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("body", sqlalchemy.String),
+    sqlalchemy.Column("body", sqlalchemy.String)
 )
 
 comment_table = sqlalchemy.Table(
@@ -17,10 +17,14 @@ comment_table = sqlalchemy.Table(
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column("body", sqlalchemy.String),
-    sqlalchemy.Column("post_id", sqlalchemy.ForeignKey("posts.id"), nullable=False),
+    sqlalchemy.Column("post_id", sqlalchemy.ForeignKey("posts.id"), nullable=False)
 )
 
-engine = sqlalchemy.create_engine(config.DATABASE_URL, connect_args={"check_same_thread": False})
+engine = sqlalchemy.create_engine(
+    config.DATABASE_URL, connect_args={"check_same_thread": False}
+)
 
 metadata.create_all(engine)
-database = databases.Database(config.DATABASE_URL, force_rollback=config.DB_FORCE_ROLL_BACK)
+database = databases.Database(
+    config.DATABASE_URL, force_rollback=config.DB_FORCE_ROLL_BACK
+)
