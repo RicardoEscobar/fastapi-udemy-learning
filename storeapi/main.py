9ca20adcec8041fa644fa221711b5061b8523e2e
Ricardo Escobar <jorge.ricardo.escobar@gmail.com>
2026-01-08 20:07:27 -0600

    Refactor lifespan management to connect and disconnect database in FastAPI application

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,14 +1,18 @@
 from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+
+from storeapi.database import database
 from storeapi.routers.post import router as post_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await post_router.startup()
+    await database.connect()
     yield
-    await post_router.shutdown()
-
+    await database.disconnect()
 
 app = FastAPI(lifespan=lifespan)
+
+
 app.include_router(post_router)
